d2d6636209700e5a7eab08bcecfcf3d3d702b13e
Alexander Young <alex1001@live.ca>
2025-05-17 03:27:58 -0700

    Resets server states after transfer
    Ensures server states are correctly reset to prevent conflicts
    after a transfer operation is completed. Specifically, it resets
    the 'stopping' and 'restarting' states to false, aligning the
    server's internal state with its actual operational status.

--- classes/server.js
+++ classes/server.js
@@ -636,6 +636,8 @@ class Server extends Module {
         if (this.state.transfering && this.main.activeTransfers.has(this.config.id) && this.main.activeTransfers.get(this.config.id).direction == "source") {
             this.log("Server Transfering", this.main.lp({ color: 2 }));
             this.state.transfering = false;
+            this.state.stopping = false;
+            this.state.restarting = false;
             this.uninstall();
             this.main.activeTransfers.get(this.config.id).state = "Ready";
             this.main.activeTransfers.delete(this.config.id);
